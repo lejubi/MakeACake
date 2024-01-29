604cb2ed1bd7feef0d1874cddd4126e5a68e5c9d
lejubi <junalee515@gmail.com>
2024-01-29 16:03:37 -0500

    made index.js file unefficiently and sending data to outro.html still doesnt work

---
+++ index.js
@@ -0,0 +1,19 @@
+var cakeChoice = 1;
+var frostingChoice = 0;
+var toppingChoice = 0;
+
+function cakeChoice1() {cakeChoice = 1; console.log(cakeChoice)}
+function cakeChoice2() {cakeChoice = 2; console.log(cakeChoice)}
+function cakeChoice3() {cakeChoice = 3; console.log(cakeChoice)}
+
+function frostingChoice0() {frostingChoice = 0; console.log(frostingChoice)}
+function frostingChoice1() {frostingChoice = 1; console.log(frostingChoice)}
+function frostingChoice2() {frostingChoice = 2; console.log(frostingChoice)}
+function frostingChoice3() {frostingChoice = 3; console.log(frostingChoice)}
+
+function toppingChoice0() {toppingChoice = 0; console.log(toppingChoice)}
+function toppingChoice1() {toppingChoice = 1; console.log(toppingChoice)}
+function toppingChoice2() {toppingChoice = 2; console.log(toppingChoice)}
+function toppingChoice3() {toppingChoice = 3; console.log(toppingChoice)}
+function toppingChoice4() {toppingChoice = 4; console.log(toppingChoice)}
+function toppingChoice5() {toppingChoice = 5; console.log(toppingChoice)}
