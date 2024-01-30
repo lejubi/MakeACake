c355d4f84cb25aef6a7d4e8fd8c0fc83591b2635
lejubi <junalee515@gmail.com>
2024-01-29 20:52:37 -0500

    sending info to outro.html works

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ var cakeChoice = 1;
 var frostingChoice = 0;
 var toppingChoice = 0;
 
-function cakeChoice1() {cakeChoice = 1; console.log(cakeChoice)}
-function cakeChoice2() {cakeChoice = 2; console.log(cakeChoice)}
-function cakeChoice3() {cakeChoice = 3; console.log(cakeChoice)}
+function cakeChoice1() {localStorage.setItem("cakeChoice", "1"); console.log(localStorage.getItem("cakeChoice"))}
+function cakeChoice2() {localStorage.setItem("cakeChoice", "2"); console.log(localStorage.getItem("cakeChoice"))}
+function cakeChoice3() {localStorage.setItem("cakeChoice", "3"); console.log(localStorage.getItem("cakeChoice"))}
 
-function frostingChoice0() {frostingChoice = 0; console.log(frostingChoice)}
-function frostingChoice1() {frostingChoice = 1; console.log(frostingChoice)}
-function frostingChoice2() {frostingChoice = 2; console.log(frostingChoice)}
-function frostingChoice3() {frostingChoice = 3; console.log(frostingChoice)}
+function frostingChoice0() {localStorage.setItem("frostingChoice", "0"); console.log(localStorage.getItem("frostingChoice"))}
+function frostingChoice1() {localStorage.setItem("frostingChoice", "1"); console.log(localStorage.getItem("frostingChoice"))}
+function frostingChoice2() {localStorage.setItem("frostingChoice", "2"); console.log(localStorage.getItem("frostingChoice"))}
+function frostingChoice3() {localStorage.setItem("frostingChoice", "3"); console.log(localStorage.getItem("frostingChoice"))}
 
-function toppingChoice0() {toppingChoice = 0; console.log(toppingChoice)}
-function toppingChoice1() {toppingChoice = 1; console.log(toppingChoice)}
-function toppingChoice2() {toppingChoice = 2; console.log(toppingChoice)}
-function toppingChoice3() {toppingChoice = 3; console.log(toppingChoice)}
-function toppingChoice4() {toppingChoice = 4; console.log(toppingChoice)}
-function toppingChoice5() {toppingChoice = 5; console.log(toppingChoice)}
+function toppingChoice0() {localStorage.setItem("toppingChoice", "0"); console.log(localStorage.getItem("toppingChoice"))}
+function toppingChoice1() {localStorage.setItem("toppingChoice", "1"); console.log(localStorage.getItem("toppingChoice"))}
+function toppingChoice2() {localStorage.setItem("toppingChoice", "2"); console.log(localStorage.getItem("toppingChoice"))}
+function toppingChoice3() {localStorage.setItem("toppingChoice", "3"); console.log(localStorage.getItem("toppingChoice"))}
+function toppingChoice4() {localStorage.setItem("toppingChoice", "4"); console.log(localStorage.getItem("toppingChoice"))}
+function toppingChoice5() {localStorage.setItem("toppingChoice", "5"); console.log(localStorage.getItem("toppingChoice"))}
